b613c701c086cad0ec7826c7347038ab827a49cf
Romiec13 <romiec13@gmail.com>
2022-02-03 10:59:56 -0600

    Added current date to JavaScript

--- src/script.js
+++ src/script.js
@@ -1,7 +1,25 @@
-let apiKey = "ecfb1152c5a567d13565473b15c3239c";
-let city = "Moscow";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-console.log(apiUrl);
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 
 function dispalyInformation(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -9,11 +27,18 @@ function dispalyInformation(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
+
+let apiKey = "ecfb1152c5a567d13565473b15c3239c";
+let city = "Houston";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
 axios.get(apiUrl).then(dispalyInformation);
