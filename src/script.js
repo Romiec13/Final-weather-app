393fd3c04d8a83ad021cdc5e193f4849cf68c144
Romiec13 <romiec13@gmail.com>
2022-02-10 10:46:28 -0600

    Added weather forecast API to JS

--- src/script.js
+++ src/script.js
@@ -21,7 +21,9 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function dipslayForecast() {
+function displayForecast(response) {
+console.log(response.data.daily);
+
   let forecastElement= document.querySelector("#forecast");
   let forecastHTML= `<div class="row">`;
   let days=["Thu", "Fri", "Sat", "Sun", "Mon"];
@@ -44,6 +46,12 @@ function dipslayForecast() {
   forecastElement.innerHTML= forecastHTML;
 }
 
+function getForecast (coordinates) {
+  let apiKey="ecfb1152c5a567d13565473b15c3239c";
+  let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function displayInformation(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -67,6 +75,8 @@ function displayInformation(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -111,7 +121,7 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemp);
 
 search("Houston");
-dipslayForecast();
+
 
 function searchLocation(position) {
   let apiKey = "ecfb1152c5a567d13565473b15c3239c";
